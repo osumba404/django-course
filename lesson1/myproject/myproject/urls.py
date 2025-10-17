f017993ee48cedaa6b27fc577f4239933d02a4c6
Evans Osumba <osumbaevanzz@gmail.com>
2025-10-17 21:04:39 +0300

    day 3

--- lesson1/myproject/myproject/urls.py
+++ lesson1/myproject/myproject/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('about/', views.about),
     path('posts/', include('posts.urls')),
     path('users/', include('users.urls')),
+
 ]
 
 
--- lesson1/myproject/users/urls.py
+++ lesson1/myproject/users/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),    
+    path('register/', views.register_view, name='register'), 
+    path('register/', views.login_view, name='login'), 
+       
 ]
